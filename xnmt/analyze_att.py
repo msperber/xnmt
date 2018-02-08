67a26c0347ceb6d025259e0090d7d1afbf2ea8a6
Matthias Sperber <matthias.sperber@gmail.com>
2018-02-08 17:54:59 +0100

    dot product + visualization code

--- xnmt/analyze_att.py
+++ xnmt/analyze_att.py
@@ -1,14 +1,20 @@
 from collections import  Counter
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
 import numpy as np
 
 import yaml
 
 from xnmt.vocab import Vocab
 
-vocab = Vocab(vocab_file = "/project/data-audio/tedlium-multi/parallel/vocab/en-de-es-fr.lc.no-numbers-punct.vocab") # /Users/matthias/Desktop/en-de-es-fr.lc.no-numbers-punct.vocab
 nheads = 8
 nlayers = 2
+vocab = Vocab(vocab_file = "/project/data-audio/tedlium-multi/parallel/vocab/en-de-es-fr.lc.no-numbers-punct.vocab")
+# vocab = Vocab(vocab_file = "/Users/matthias/Desktop/en-de-es-fr.lc.no-numbers-punct.vocab")
 
 with open("/project/iwslt2015b/project/nmt-audio/exp-xnmt/03.audio2char/logs/exp165a.att.1.log.yaml", 'r') as f:
 #with open("/Users/matthias/Desktop/exp165a.att.1.log.small.yaml", 'r') as f:
@@ -37,8 +43,10 @@ with open("/project/iwslt2015b/project/nmt-audio/exp-xnmt/03.audio2char/logs/exp
           cross_att_sum = np.sum([np.loads(sent_log[i]["value"]) for i in range(len(sent_log)-1) if sent_log[i]["key"]=="attention" and sent_log[i+1]["value"]==token_i],axis=0)
           if cross_att_sum.shape:
             cross_att_sum = np.repeat(cross_att_sum, 2)
-            ca0 = np.corrcoef(self_att_ax0, cross_att_sum)[0,1]
-            ca1 = np.corrcoef(self_att_ax1, cross_att_sum)[0,1]
+#             ca0 = np.corrcoef(self_att_ax0, cross_att_sum)[0,1]
+#             ca1 = np.corrcoef(self_att_ax1, cross_att_sum)[0,1]
+            ca0 = np.dot(self_att_ax0, cross_att_sum)
+            ca1 = np.dot(self_att_ax1, cross_att_sum)
             axis0_sum[(layer_i,head_i,vocab[token_i])] += ca0
             axis1_sum[(layer_i,head_i,vocab[token_i])] += ca1
             axis0_cnt[(layer_i,head_i,vocab[token_i])] += 1
@@ -62,7 +70,7 @@ with open("/project/iwslt2015b/project/nmt-audio/exp-xnmt/03.audio2char/logs/exp
   print("-----")
   print(axis1_avg_arr)
   print("-----")
-  with open("/home/msperber/experiments/attention-heads/165.1.yaml", "w") as f_out:
+  with open("/home/msperber/experiments/attention-heads/165.2.dotprod.yaml", "w") as f_out:
     f_out.write(yaml.dump({
     "axis0_avg":axis0_avg,
     "axis1_avg":axis1_avg,
@@ -70,7 +78,48 @@ with open("/project/iwslt2015b/project/nmt-audio/exp-xnmt/03.audio2char/logs/exp
     "axis1_avg_arr":axis1_avg_arr
     }))
 
+# with open("/Users/matthias/Desktop/165.1.yaml", 'r') as f:
+#   yaml_results = yaml.load(f)
+#   data = yaml_results["axis1_avg_arr"]
+#   valid_vocab = [vocab[i] for i in range(len(vocab)) if sum(data[:,i]!=0.0)]
+#   valid_vocab_indices = [i for i in range(len(vocab)) if sum(data[:,i]!=0.0)]
+#   data_sel = data[:,[i for i in range(data.shape[1]) if sum(data[:,i]!=0.0)]] # drop vocab entries that never occurred
+#   fig = plt.figure()
+#   ax = fig.add_subplot(111)
+#   ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+#   ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+#   ax.set_xticklabels(['']+valid_vocab, fontsize=8, minor=True)
+#   ax.set_xticklabels(['']*20, fontsize=8)
+#   ax.matshow(data_sel)
+#   ax.set_aspect('auto')
+#   fig.savefig("/Users/matthias/Desktop/165.1.png", dpi=120)
+#   fig.clf()
+#   plt.close('all')
+#   
+#   for head_i in range(16):
+#     print([vocab[v_i] for v_i in sorted(valid_vocab_indices, key=lambda i: -data[head_i,i])])
+  
+
+
+
 
 # clustering: check
 # - https://stackoverflow.com/questions/2455761/reordering-matrix-elements-to-reflect-column-and-row-clustering-in-naiive-python
 # - http://scikit-learn.org/stable/modules/biclustering.html
+
+#   from sklearn.datasets import make_biclusters
+#   from sklearn.datasets import samples_generator as sg
+#   from sklearn.cluster.bicluster import SpectralCoclustering
+#   from sklearn.metrics import consensus_score
+#   model = SpectralCoclustering(n_clusters=5, random_state=0)
+#   model.fit(yaml_results["axis1_avg_arr"])
+#   fit_data = yaml_results["axis1_avg_arr"][np.argsort(model.row_labels_)]
+#   fit_data = fit_data[:, np.argsort(model.column_labels_)]
+#   fig = plt.figure()
+#   ax = fig.add_subplot(111)
+#   ax.matshow(fit_data)
+#   ax.set_aspect('auto')
+#   fig.savefig("/Users/matthias/Desktop/165.1.cluster.png", dpi=1200)
+#   fig.clf()
+#   plt.close('all')
+
