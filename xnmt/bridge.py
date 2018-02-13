4656219d3f6b973ad6f731880a6aaf5d0d4feb62
Matthias Sperber <matthias.sperber@gmail.com>
2018-02-13 09:25:25 +0100

    fix copy bridge dim check

--- xnmt/bridge.py
+++ xnmt/bridge.py
@@ -37,9 +37,9 @@ class CopyBridge(Bridge, Serializable):
     self.dec_dim = dec_dim or exp_global.default_layer_dim
   def decoder_init(self, enc_final_states):
     if self.dec_layers > len(enc_final_states):
-      raise RuntimeError("CopyBridge requires dec_layers <= len(enc_final_states), but got %s and %s" % (self.dec_layers, len(enc_final_states)))
-    if enc_final_states[0].main_expr().dim()[0][0] != self.dec_dim:
-      raise RuntimeError("CopyBridge requires enc_dim == dec_dim, but got %s and %s" % (enc_final_states[0].main_expr().dim()[0][0], self.dec_dim))
+      raise RuntimeError(f"CopyBridge requires dec_layers <= len(enc_final_states), but got {self.dec_layers} and {len(enc_final_states)}")
+    if enc_final_states[-1].main_expr().dim()[0][0] != self.dec_dim:
+      raise RuntimeError(f"CopyBridge requires enc_dim == dec_dim, but got {enc_final_states[-1].main_expr().dim()[0][0]} and {self.dec_dim}")
     return [enc_state.cell_expr() for enc_state in enc_final_states[-self.dec_layers:]] \
          + [enc_state.main_expr() for enc_state in enc_final_states[-self.dec_layers:]]
 
