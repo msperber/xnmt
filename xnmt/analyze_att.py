069a4c18d03394fd0cf68ffc6693ec759a887452
Matthias Sperber <matthias.sperber@gmail.com>
2018-02-20 11:08:33 +0100

    analyze_att: more metrics, compute jointly over all sequences

--- xnmt/analyze_att.py
+++ xnmt/analyze_att.py
@@ -1,11 +1,11 @@
 import argparse
-from collections import  Counter
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import scipy.spatial.distance
+import scipy.stats
 import numpy as np
 
 import yaml
@@ -17,7 +17,7 @@ parser.add_argument('--vocab', type=str, default="/Users/matthias/Desktop/en-de-
 parser.add_argument('--yaml_log', type=str, default="/Users/matthias/Desktop/165.3.cossim.yaml")
 parser.add_argument('--summarize_yaml', type=str, default="/Users/matthias/Desktop/165.3.cossim.yaml")
 parser.add_argument('--plot', type=str, default="/Users/matthias/Desktop/165.3.png")
-parser.add_argument('--distance', type=str, default="corrcoef") # corrcoef | cosine
+parser.add_argument('--distance', type=str, default="-corrcoef") # corrcoef | cosine | intersection | urelent | urelent2 | spearman
 parser.add_argument('--do_summarize', dest='do_summarize', action='store_const',
                     const=True, default=False,)
 parser.add_argument('--do_plot', dest='do_plot', action='store_const',
@@ -48,14 +48,26 @@ def plot_mat(mat, filename, x_labels=[], dpi=120, fontsize=6):
   ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
   ax.set_xticklabels(['']+x_labels, fontsize=fontsize, minor=True)
   ax.set_xticklabels(['']*20, fontsize=fontsize)
-  ax.matshow(mat)
+  cax = ax.matshow(mat)
+  fig.colorbar(cax)
   ax.set_aspect('auto')
   fig.savefig(filename, dpi=dpi)
   fig.clf()
   plt.close('all')
 
 
-
+def dist(a, b, metric):
+  if metric=="corrcoef":
+    return -np.corrcoef(a, b)[0,1]
+  elif metric=="cosine":
+    return scipy.spatial.distance.cosine(a, b)
+  elif metric=="intersection":
+    return np.sum(np.minimum(a, b))
+  elif metric=="urelent":
+    return np.sum(np.multiply(a, np.log(np.divide(a, b)) + b - a))
+  elif metric=="spearman":
+    return -scipy.stats.spearmanr(a, b).correlation
+  else: raise ValueError("unknown metric {}".format(metric))
 
 if should_summarize_log:
   with open(yaml_log_file, 'r') as f:
@@ -68,77 +80,66 @@ if should_summarize_log:
         sentence_logs.append([])
       sentence_logs[-1].append(entry)
       prev_key = cur_key
-    axis0_sum, axis1_sum, axis0_cnt, axis1_cnt = Counter(), Counter(), Counter(), Counter() 
-    for sent_log in sentence_logs:
-      for layer_i in range(nlayers):
-        for head_i in range(nheads):
-          # shape layer 0: (downsampled_src_len*2,)
-          # shape layer 1: (downsampled_src_len,)
-          self_att_ax0 = np.loads(sent_log[layer_i*2]["value"])[:,head_i]
-          self_att_ax1 = np.loads(sent_log[layer_i*2+1]["value"])[:,head_i]
-          if layer_i==1:
-            self_att_ax0 = np.repeat(self_att_ax0, 2)
-            self_att_ax1 = np.repeat(self_att_ax1, 2)
-          for token_i in range(len(vocab)):
+      
+    axis0_stats, axis1_stats = {}, {}
+    for layer_i in range(nlayers):
+      for head_i in range(nheads):
+        for token_i in range(len(vocab)):
+          for sent_log in sentence_logs:
+            axis0_concat, axis1_concat, cross_att_sum_concat = None, None, None 
+            # shape layer 0: (downsampled_src_len*2,)
+            # shape layer 1: (downsampled_src_len,)
+            self_att_ax0 = np.loads(sent_log[layer_i*2]["value"])[:,head_i]
+            self_att_ax1 = np.loads(sent_log[layer_i*2+1]["value"])[:,head_i]
+            if layer_i==1:
+              self_att_ax0 = np.repeat(self_att_ax0, 2)
+              self_att_ax1 = np.repeat(self_att_ax1, 2)
             # cross_att_sum.shape = (downsampled_src_len, 1)
             for i in range(len(sent_log)-1):
               if sent_log[i]["key"] == "attention":
-                if sent_log[i]["key"] != "forced_dec_id":
+                if sent_log[i+1]["key"] != "forced_dec_id":
                   raise ValueError("didn't find key 'forced_dec_id' after key 'attention', maybe this was not created using forced decoding?")
             cross_att_sum = np.sum([np.loads(sent_log[i]["value"]) for i in range(len(sent_log)-1) if sent_log[i]["key"]=="attention" and sent_log[i+1]["value"]==token_i],axis=0)
             if cross_att_sum.shape:
               cross_att_sum = np.repeat(cross_att_sum, 2)
-              if args.distance=="corrcoef":
-                ca0 = np.corrcoef(self_att_ax0, cross_att_sum)[0,1]
-                ca1 = np.corrcoef(self_att_ax1, cross_att_sum)[0,1]
-              elif args.distance=="cosine":
-#               ca0 = np.dot(self_att_ax0, cross_att_sum)
-#               ca1 = np.dot(self_att_ax1, cross_att_sum)
-                ca0 = scipy.spatial.distance.cosine(self_att_ax0, cross_att_sum)
-                ca1 = scipy.spatial.distance.cosine(self_att_ax1, cross_att_sum)
-              else: raise ValueError("unknown distance {}".format(args.distance))
-              axis0_sum[(layer_i,head_i,vocab[token_i])] += ca0
-              axis1_sum[(layer_i,head_i,vocab[token_i])] += ca1
-              axis0_cnt[(layer_i,head_i,vocab[token_i])] += 1
-              axis1_cnt[(layer_i,head_i,vocab[token_i])] += 1
-    axis0_avg, axis1_avg = Counter(), Counter()
-    axis0_avg_arr, axis1_avg_arr = np.zeros((nheads*nlayers, len(vocab))), np.zeros((nheads*nlayers, len(vocab)))
-    for layer_i in range(nlayers):
-      for head_i in range(nheads):
-        for token_i in range(len(vocab)):
-          ind = (layer_i,head_i,vocab[token_i])
-          if ind in axis0_cnt: 
-            axis0_avg[ind] = axis0_sum[ind] / axis0_cnt[ind]
-            axis1_avg[ind] = axis1_sum[ind] / axis1_cnt[ind]
-            axis0_avg_arr[nheads*layer_i+head_i,token_i] = axis0_avg[ind]
-            axis1_avg_arr[nheads*layer_i+head_i,token_i] = axis1_avg[ind]
-    print(axis0_avg)
+              if axis0_concat:
+                axis0_concat = np.concatenate((axis0_concat, self_att_ax0))
+                axis1_concat = np.concatenate((axis1_concat, self_att_ax1))
+                cross_att_sum_concat = np.concatenate((cross_att_sum_concat, cross_att_sum))
+              else:
+                axis0_concat = self_att_ax0
+                axis1_concat = self_att_ax1
+                cross_att_sum_concat = cross_att_sum
+          if axis0_concat is not None:
+            ca0 = dist(axis0_concat, cross_att_sum_concat, args.distance)
+            axis0_stats[(layer_i,head_i,vocab[token_i])] = float(ca0)
+            ca1 = dist(axis1_concat, cross_att_sum_concat, args.distance)
+            axis1_stats[(layer_i,head_i,vocab[token_i])] = float(ca1)
+    print(axis0_stats)
     print("-----")
-    print(axis1_avg)
-    print("-----")
-    print(axis0_avg_arr)
-    print("-----")
-    print(axis1_avg_arr)
-    print("-----")
+    print(axis1_stats)
     with open(summarized_yaml_file, "w") as f_out:
       f_out.write(yaml.dump({
-      "axis0_avg":axis0_avg,
-      "axis1_avg":axis1_avg,
-      "axis0_avg_arr":axis0_avg_arr,
-      "axis1_avg_arr":axis1_avg_arr
+      "axis0_stats":axis0_stats,
+      "axis1_stats":axis1_stats,
       }))
 
 if should_plot:
   with open(summarized_yaml_file, 'r') as f:
     yaml_results = yaml.load(f)
-    data = yaml_results["axis0_avg_arr"]
-    valid_vocab = [vocab[i] for i in range(len(vocab)) if sum(data[:,i]!=0.0)]
-    valid_vocab_indices = [i for i in range(len(vocab)) if sum(data[:,i]!=0.0)]
-    data_sel = data[:,[i for i in range(data.shape[1]) if sum(data[:,i]!=0.0)]] # drop vocab entries that never occurred
-    plot_mat(data_sel, plot_file, valid_vocab)
+    data = yaml_results["axis0_stats"]
+    valid_vocab = [vocab[k] for k in range(len(vocab)) if any((i,j,vocab[k]) in data for i in range(nlayers) for j in range(nheads))]
+    valid_vocab_indices = [k for k in range(len(vocab)) if any((i,j,vocab[k]) in data for i in range(nlayers) for j in range(nheads))]
+    mat = np.zeros((nlayers*nheads, len(valid_vocab)))
+    for i in range(nlayers):
+      for j in range(nheads):
+        for v in valid_vocab:
+          mat[nheads*i+j,valid_vocab.index(v)] = -data[(i,j,v)]
+    plot_mat(mat, plot_file, valid_vocab)
      
-    for head_i in range(16):
-      print([vocab[v_i] for v_i in sorted(valid_vocab_indices, key=lambda i: -data[head_i,i])])
+    for i in range(nlayers):
+      for j in range(nheads):
+        print([v for v in sorted(valid_vocab, key=lambda k: data[(i,j,k)])])
   
 
 
