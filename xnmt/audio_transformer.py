fe1bbebbc210c8c1f4be965953e61ae2f2ae27f3
Matthias Sperber <matthias.sperber@gmail.com>
2018-03-12 15:07:39 +0100

    disabled batch norm for self-att interleaved nin

--- xnmt/audio_transformer.py
+++ xnmt/audio_transformer.py
@@ -397,7 +397,7 @@ class TransformerEncoderLayer(object):
                                               glorot_gain=glorot_gain)
     elif ff_nin:
       self.feed_forward = NetworkInNetworkBiLSTMTransducer(exp_global=exp_global, layers=1, input_dim=hidden_dim,
-                                              hidden_dim=hidden_dim, batch_norm=True, dropout=dropout,
+                                              hidden_dim=hidden_dim, batch_norm=False, dropout=dropout,
                                               glorot_gain_lstm=glorot_gain, glorot_gain_nin=glorot_gain)
     elif ff_window==1:
       self.feed_forward = PositionwiseFeedForward(hidden_dim, ff_hidden_dim, model, nonlinearity=nonlinearity,
