4cb6c233ff35535abc3b09837111669304c140c7
Matthias Sperber <matthias.sperber@gmail.com>
2018-04-17 10:28:13 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ from Cython.Build import cythonize
 import sys
 
 sys.path.append("./xnmt")
-sys.path.append("./xnmt/serialize")
-sys.path.append("./xnmt/speech_features")
 
 with open("requirements.txt", encoding="utf-8") as req_fp:
   install_requires = req_fp.readlines()
@@ -33,7 +31,7 @@ setup(
   license='Apache License',
   install_requires=install_requires,
   packages=[
-      'xnmt','xnmt.serialize','xnmt.speech_features'
+      'xnmt'
   ],
   ext_modules=ext_modules,
 )
